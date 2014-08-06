20f272e1e1116a4a20c529df21fc5e12e4770b2a
Dmitry Tikhonov <tikhonovdma@gmail.com>
2014-08-06 23:56:46 +0400

    add handler returning directory content as json array

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject divisus "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Artifact repository"
+  :url "https://github.com/Finiriarh/divisus"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring/ring-core "1.3.0"]
+                 [ring/ring-jetty-adapter "1.3.0"]]
   :main ^:skip-aot divisus.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/divisus/core.clj
+++ src/divisus/core.clj
@@ -1,7 +1,23 @@
 (ns divisus.core
-  (:gen-class))
+	(:require [ring.adapter.jetty     :refer [run-jetty]]
+		        [ring.middleware.params :refer [wrap-params]]
+		        [clojure.java.io        :refer [file]])
+	(:gen-class))
+
+(defn ls [path] 
+	(seq (.list (file path))))
+
+(defn json-array [seq]
+	(str "[" (apply str (interpose ", " (map #(str "\"" % "\"") seq))) "]"))
+(defn handler [{{path "path"} :params}]
+	{:status 200
+		:headers {"Content-Type" "application/json"}
+		:body (json-array (ls path))})
+
+(def app
+  (-> handler wrap-params))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+	"Starts jetty server"
+	[& args]
+	(run-jetty app {:port 3131}))
