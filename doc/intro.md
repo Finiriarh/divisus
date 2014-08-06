2714c0d8411ad9f6ddddf224e00be832dc89c8a9
Dmitry Tikhonov <tikhonovdma@gmail.com>
2014-08-06 22:21:18 +0400

    initial template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to divisus
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/divisus/core_test.clj
@@ -0,0 +1,7 @@
+(ns divisus.core-test
+  (:require [clojure.test :refer :all]
+            [divisus.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
